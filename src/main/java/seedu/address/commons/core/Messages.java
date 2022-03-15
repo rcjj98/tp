d67c3da5273a098e06fd1bbda1d37162778c03f0
Ryan Chang Jia Jie <>
2022-03-15 17:59:23 +0800

    Add 2 messages to be thrown as ParseExceptions

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -9,5 +9,10 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_INVALID_STAGE_FORMAT = "Invalid stage provided!";
+    public static final String MESSAGE_MISSING_STAGE_FIELD = "Please include a stage for the job application.";
+
+
+
 
 }
