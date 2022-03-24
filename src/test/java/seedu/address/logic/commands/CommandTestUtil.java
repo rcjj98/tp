b11403061b7c471378140d7e14d01642ca3ca596
Ryan Chang Jia Jie <>
2022-03-24 17:58:44 +0800

    Update valid application inputs in CommandTestUtil.java

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -35,8 +35,8 @@ public class CommandTestUtil {
     public static final String VALID_EMAIL_BOB = "bob@example.com";
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
-    public static final String VALID_APPLICATION_HUSBAND = "1";
-    public static final String VALID_APPLICATION_FRIEND = "2";
+    public static final String VALID_APPLICATION_HUSBAND = "1 ACCEPTED";
+    public static final String VALID_APPLICATION_FRIEND = "2 INPROGRESS";
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
