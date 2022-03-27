f506fc20cced623c2422106d8cfb092d1d42bcff
brigittesantoso <brigittepsantoso@gmail.com>
2022-03-27 18:08:13 +0800

    Revert "Revert "Add clear interivew command""
    This reverts commit 31acb9b29e4237ae687186d46b598b284a9043f3.

--- src/main/java/seedu/address/logic/commands/ClearInterviewCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearInterviewCommand.java
@@ -2,18 +2,17 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.Type.INTERVIEW;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_INTERVIEWS;
 
 import seedu.address.logic.parser.Type;
 import seedu.address.model.Model;
 
-public class ListInterviewCommand extends ListCommand {
-    public static final String MESSAGE_SUCCESS = "Listed all interviews!";
+public class ClearInterviewCommand extends ClearCommand {
+    public static final String MESSAGE_SUCCESS = "Interviews have all been cleared!";
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredInterviewList(PREDICATE_SHOW_ALL_INTERVIEWS);
+        model.resetInterviews();
         return new CommandResult(MESSAGE_SUCCESS, getType());
     }
 
@@ -22,5 +21,4 @@ public class ListInterviewCommand extends ListCommand {
         return INTERVIEW;
     }
 }
-
 
