eb3701e0bd7057ff73815aecdc30d29caea0ce90
Ryan Chang Jia Jie <>
2022-03-31 16:48:53 +0800

    Updated ExportCommand.java and ExportCommandParser.java to export correctly

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -2,13 +2,20 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import javafx.collections.ObservableList;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 
 public class ExportCommand extends Command {
     public static final String COMMAND_WORD = "export";
     public static final String MESSAGE_USAGE = COMMAND_WORD
         + ": Exports all data from all job applicants to a CSV file\n";
+    public static final String MISSING_JSON_FILE = "Json file containing job applicant data not found.";
 
     private final String strCsvFilePath;
 
@@ -24,7 +31,31 @@ public class ExportCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return new CommandResult("Exported job applicants' data to " + strCsvFilePath, getType());
+        requireNonNull(model);
+        ObservableList<Person> personList = model.getFilteredPersonList(); //to read from json and write to CSV
+
+        try {
+            //create new csv file to export data into
+            File csvFile = new File(strCsvFilePath);
+            PrintWriter pw = new PrintWriter(csvFile);
+
+            //export all persons data from addressbook.json into specified csv file
+            for (Person p : personList) {
+                String name = p.getName().toString();
+                String phone = p.getPhone().toString();
+                String email = p.getEmail().toString();
+                String address = p.getAddress().toString();
+                String job = p.getJob().toString();
+                String stage = p.getStage().toString();
+                pw.printf("%s\t%s\t%s\t%s\t%s\t%s\n", name, phone, email, address, job, stage);
+            }
+            pw.close();
+
+        } catch (FileNotFoundException e) {
+            throw new CommandException(MISSING_JSON_FILE);
+        }
+        return new CommandResult("Exported " + personList.size() + " job applicants' data to "
+            + strCsvFilePath, getType());
     }
 
 }
--- src/main/java/seedu/address/logic/parser/ExportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ExportCommandParser.java
@@ -3,29 +3,14 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.util.FileUtil.isValidPath;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Optional;
-
-import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.logic.commands.ExportCommand;
 import seedu.address.logic.commands.ImportCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.person.Person;
-import seedu.address.storage.JsonAddressBookStorage;
 
 public class ExportCommandParser implements Parser<ExportCommand> {
 
     public static final String INVALID_FILE_PATH = "That is not a valid file path\n"
         + "Please check for any illegal characters.";
-    public static final String INVALID_JSON_FILE_FORMAT = "Error while parsing JSON file\n "
-        + "Please ensure that all data fields is correct.";
-    public static final String MISSING_JSON_FILE = "Json file containing job applicant data not found.";
     public static final String WRONG_FILE_TYPE = "No json file extension found.";
 
     /**
@@ -40,49 +25,7 @@ public class ExportCommandParser implements Parser<ExportCommand> {
         String strCsvFilePath = args.strip();
         checkFilePath(strCsvFilePath);
 
-        return writeToCsvFile(strCsvFilePath);
-
-    }
-
-    /**
-     * Writes the details of every job applicant into the specified csv file in the path.
-     *
-     * @param filePath String filepath to the target csv file to export data into
-     * @return ExportCommand with the same specified filePath.
-     * @throws ParseException If addressbook.json is missing or has invalid json format.
-     */
-    public ExportCommand writeToCsvFile(String filePath) throws ParseException {
-
-        try {
-            //create new csv file to export data into
-            File csvFile = new File(filePath);
-            PrintWriter pw = new PrintWriter(csvFile);
-
-            //retrieve data from current addressbook.json file
-            Path addressBookFilePath = Paths.get("data", "addressbook.json");
-            Optional<ReadOnlyAddressBook> newAddrBook;
-            newAddrBook = new JsonAddressBookStorage(addressBookFilePath).readAddressBook();
-
-            //export all persons data from addressbook.json into csv file
-            List<Person> personList = newAddrBook.get().getPersonList();
-            for (Person p : personList) {
-                String name = p.getName().toString();
-                String phone = p.getPhone().toString();
-                String email = p.getEmail().toString();
-                String address = p.getAddress().toString();
-                String job = p.getJob().toString();
-                String stage = p.getStage().toString();
-                pw.printf("%s\t%s\t%s\t%s\t%s\t%s\n", name, phone, email, address, job, stage);
-            }
-            pw.close();
-            return new ExportCommand(filePath);
-
-        } catch (FileNotFoundException e) {
-            throw new ParseException(MISSING_JSON_FILE);
-
-        } catch (DataConversionException e) {
-            throw new ParseException(INVALID_JSON_FILE_FORMAT);
-        }
+        return new ExportCommand(strCsvFilePath);
     }
 
     /**
